3cf411bd86189423d99112228d35936dee3295a4
Hetvi <hetvigodhani@vebuin.com>
2025-03-03 14:25:42 +0530

    removed db_crud_orm unwanted files

--- NODE/DB_CRUD_ORM/src/domain/schemas/updateSchema.ts
+++
@@ -1,8 +0,0 @@
-import Joi from 'joi';
-
-export const updateSchema = Joi.object({
-    id:Joi.number().required(),
-    email:Joi.string().email(),
-    username: Joi.string().min(3).max(30),
-    password: Joi.string().min(6),
-});
--- NODE/DB_CRUD_ORM/src/infrastructure/helpers/middleware/transaction.ts
+++
@@ -1,8 +0,0 @@
-import { EntityManager } from "typeorm/entity-manager/EntityManager"
-import { AppDataSource } from "../orm/typeorm/config/ormconfig.ts"
-
-export const wrapTransaction=async<T>(fun:(t:EntityManager)=>Promise<T>):Promise<T>=>{
-    return await AppDataSource.transaction(async (transactionalEntityManager) => {
-        return fun(transactionalEntityManager)
-    })
-}
